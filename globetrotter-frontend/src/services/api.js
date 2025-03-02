91b17c457ad561ebf42963ad50c3fee85e88fe3e
deeppatel007 <deepp0254@gmail.com>
2025-03-02 18:06:41 +0530

    api added

--- globetrotter-frontend/src/services/api.js
+++ globetrotter-frontend/src/services/api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const API_BASE_URL = 'https://globetrotter-backend-plus-latest.onrender.com';
 
 const api = {
-  getRandomDestination: () => axios.get(`${API_BASE_URL}/questions/random`),
-  registerUser: (user) => axios.post(`${API_BASE_URL}/users/register`, user),
-  getUser: (username) => axios.get(`${API_BASE_URL}/users/${username}`),
-  updateUserScore: (username, user) => axios.put(`${API_BASE_URL}/users/${username}/score`, user),
+  getRandomDestination: () => axios.get(`${API_BASE_URL}/api/questions/random`),
+  registerUser: (user) => axios.post(`${API_BASE_URL}/api/users/register`, user),
+  getUser: (username) => axios.get(`${API_BASE_URL}/api/users/${username}`),
+  updateUserScore: (username, user) => axios.put(`${API_BASE_URL}/api/users/${username}/score`, user),
 };
 
 export default api;
