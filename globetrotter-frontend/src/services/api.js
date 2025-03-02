1446c7c287ba8d4fe07424c80bb1990905762187
deeppatel007 <deepp0254@gmail.com>
2025-03-02 17:18:30 +0530

    api

--- globetrotter-frontend/src/services/api.js
+++ globetrotter-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://globetrotter-backend-abc-latest.onrender.com';
+const API_BASE_URL = 'https://globetrotter-backend-ooo-latest.onrender.com';
 
 const api = {
   getRandomDestination: () => axios.get(`${API_BASE_URL}/questions/random`),
